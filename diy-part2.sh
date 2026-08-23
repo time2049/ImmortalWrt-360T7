2a1ae1c4ab8eac996df19afb171a54e6b4042366
time2049 <58364786+time2049@users.noreply.github.com>
2026-08-23 23:56:44 +0800

    Update default IP and hostname in config_generate

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.77.1/g' ./package/base-files/files/bin/config_generate
+# 修改默认后台 IP 为 10.1.1.1 & 主机名为 360T7
+sed -i 's/192.168.1.1/10.1.1.1/g' ./package/base-files/files/bin/config_generate
 sed -i 's/hostname="ImmortalWrt"/hostname="360T7"/g' ./package/base-files/files/bin/config_generate
 sed -i 's/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION="ImmortalWrt By IraXu $(date +%Y%m%d)"/g' ./package/base-files/files/etc/openwrt_release
